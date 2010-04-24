3388acedcd85bab63959c5bc38b666479f952dd1
Nathan Witmer <nwitmer@gmail.com>
2010-04-24 12:41:40 -0600

    install.rb

---
+++ install.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+# originally from
+# from http://errtheblog.com/posts/89-huba-huba
+
+home = File.expand_path(ENV['HOME'])
+
+Dir['*'].each do |file|
+  next if file =~ /install/
+  target = File.join(home, ".#{file}")
+  puts "installing #{File.expand_path file} to #{target}"
+  `ln -s -i #{File.expand_path file} #{target}`
+end
+
+# git push on commit
+`echo 'git push' > .git/hooks/post-commit`
+`chmod 755 .git/hooks/post-commit`
+
