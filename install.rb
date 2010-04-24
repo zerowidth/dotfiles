f592330902a9e2151f01f3254d71e82a480194e0
Nathan Witmer <nwitmer@gmail.com>
2010-04-24 12:57:41 -0600

    Tweaking install.rb

--- install.rb
+++ install.rb
@@ -1,18 +1,16 @@
 #!/usr/bin/env ruby
 
-# originally from
-# from http://errtheblog.com/posts/89-huba-huba
+# originally from from http://errtheblog.com/posts/89-huba-huba
 
 home = File.expand_path(ENV['HOME'])
 
 Dir['*'].each do |file|
-  next if file =~ /install/
+  next if file =~ /install|tags/
   target = File.join(home, ".#{file}")
-  puts "installing #{File.expand_path file} to #{target}"
-  `ln -s -i #{File.expand_path file} #{target}`
+  if File.exist?(target)
+    puts "skipping #{file}, already installed"
+  else
+    puts "installing #{File.expand_path file} to #{target}"
+    `ln -s -i #{File.expand_path file} #{target}`
+  end
 end
-
-# git push on commit
-`echo 'git push' > .git/hooks/post-commit`
-`chmod 755 .git/hooks/post-commit`
-
