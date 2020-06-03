99ec4fc5a79ff62830eaf62eb00ace32839039db
Nathan Witmer <nathan@zerowidth.com>
2020-06-03 17:05:28 -0600

    ruby installs

---
+++ ruby/install.sh
@@ -0,0 +1,17 @@
+# latest version
+latest=$(rbenv install -l | grep -E '^\d' | grep -v '-' | tail -1)
+echo "installing latest ruby: $latest"
+# rbenv install "$latest"
+# rbenv global "$latest"
+
+gem install bubs
+
+# for vim
+gem install ripper-tags
+
+# for vscode
+gem install solargraph
+gem install rubocop
+gem install ruby-debug-ide
+gem install debase
+# gem install byebug
