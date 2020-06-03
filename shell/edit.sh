0eecb49b1adeca0db2f034f160382d104846836f
Nathan Witmer <nathan@zerowidth.com>
2020-06-03 17:03:05 -0600

    Switch to vscode as default editor

---
+++ shell/edit.sh
@@ -0,0 +1,21 @@
+export EDITOR="vim"
+
+function e() {
+  if [ -n "$1" ]; then
+    if [ -d "$1" ]; then
+      # local dir
+      # dir=${1%/} # strip trailing slash so vim stays happy
+      # $SHELL -l -c "cd \"$dir\" && mvim +Refresh ."
+      code -n $1
+    else
+      # local file dir
+      # file=$(basename "$1")
+      # dir=$(dirname "$1")
+      # $SHELL -l -c "cd \"$dir\" && mvim +Refresh \"$file\""
+      code -n $1
+    fi
+  else
+    # mvim "$@"
+    code -n . "$@"
+  fi
+}
