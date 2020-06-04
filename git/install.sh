b8a204ca92f8d8d38afa70f057042e486dfc57da
Nathan Witmer <nathan@zerowidth.com>
2020-06-04 15:53:06 +0000

    Write include paths to presumed-existing global .gitconfig
    This means any preexisting file doesn't need to be overwritten. The only
    downside is that new git config changes will end up in that file instead
    of the dotfiles version, but those can be manually migrated over.

--- git/install.sh
+++ git/install.sh
@@ -1,6 +1,10 @@
-[ -z "$LINUX" ] && return
+if [ -z "$LINUX" ]; then
+  git config --global --replace-all include.path ~/.dotfiles/git/gitconfig
+  return
+fi
 
-cp git/gitconfig.nohttps ~/.gitconfig.local
+git config --global --replace-all include.path ~/.dotfiles/git/gitconfig
+git config --global --add include.path ~/.dotfiles/git/gitconfig.nohttps
 
 curl -sLo /tmp/git-delta.deb https://github.com/dandavison/delta/releases/download/0.1.1/git-delta_0.1.1_amd64.deb &&
   sudo dpkg -i /tmp/git-delta.deb
