7a0f5f9f5e268724775eb4151cb6007a98e2a566
Nathan Witmer <nathan@zerowidth.com>
2020-06-11 01:45:12 +0000

    Hardcode starship install process
    Skip the installer script entirely, this is basically what it does
    as it is.

--- starship/install.sh
+++ starship/install.sh
@@ -3,6 +3,6 @@ test -z "$LINUX" && exit 0
 if type starship >/dev/null; then
   echo "starship already installed, skipping"
 else
-  curl -fsSL https://starship.rs/install.sh -o /tmp/starship-install.sh && \
-  bash /tmp/starship-install.sh --yes
+  url="https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
+  curl --silent --fail --location "$url" | tar xzvf - -C ~/.dotfiles/bin
 fi
