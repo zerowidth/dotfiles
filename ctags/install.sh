526051cd821b3e210557ef7df38a6b8406270379
Nathan Witmer <nathan@zerowidth.com>
2021-01-28 16:03:53 -0700

    skip ctags if no autotools

--- ctags/install.sh
+++ ctags/install.sh
@@ -1,5 +1,7 @@
 test -n "$LINUX" || exit 0
 type ctags >/dev/null && exit 0
+which autoconf >/dev/null || exit 0
+
 test -d /tmp/ctags || git clone https://github.com/universal-ctags/ctags /tmp/ctags
 
 cd /tmp/ctags && \
