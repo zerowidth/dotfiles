fe878cdaf550646367f1e8cfd2a69616124f7ba1
Nathan Witmer <nathan@zerowidth.com>
2021-01-28 15:32:54 -0700

    don't need nohttps git config

--- git/install.sh
+++ git/install.sh
@@ -5,7 +5,6 @@ if [ -z "$LINUX" ]; then
 fi
 
 git config --global --replace-all include.path ~/.dotfiles/git/gitconfig
-git config --global --add include.path ~/.dotfiles/git/gitconfig.nohttps
 
 curl -sLo /tmp/git-delta.deb https://github.com/dandavison/delta/releases/download/0.1.1/git-delta_0.1.1_amd64.deb &&
   sudo dpkg -i /tmp/git-delta.deb
