579b0afd96aae2a4e5ed3dc021c5edb27d1aea86
Nathan Witmer <nathan@zerowidth.com>
2021-05-20 14:57:06 -0600

    don't reinstall ruby on mac

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,20 +1,24 @@
 [ -n "$LINUX" ] && exit 0
 
 # latest version
-latest=$(rbenv install -l | grep -E '^\d' | grep -v '-' | tail -1)
-echo "installing latest ruby: $latest"
-rbenv install "$latest"
-rbenv global "$latest"
+latest=$(rbenv install -l 2>/dev/null | grep -E '^\d' | grep -v '-' | tail -1)
+if rbenv versions | grep -q $latest; then
+  echo "ruby $latest already installed, skipping"
+else
+  echo "installing latest ruby: $latest"
+  rbenv install "$latest"
+  rbenv global "$latest"
 
-# for local scripts
-gem install bubs
+  # for local scripts
+  gem install bubs
 
-# for vim
-gem install ripper-tags
+  # for vim
+  gem install ripper-tags
 
-# for vscode
-gem install solargraph
-gem install rubocop
-gem install ruby-debug-ide
-gem install debase
-# gem install byebug
+  # for vscode
+  gem install solargraph
+  gem install rubocop
+  gem install ruby-debug-ide
+  gem install debase
+  # gem install byebug
+fi
