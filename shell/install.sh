508ef32d5a50196b0c456072d9e18be1f73c9e01
Nathan Witmer <nathan@zerowidth.com>
2021-05-20 14:57:51 -0600

    update shell utils for linux systems

--- shell/install.sh
+++ shell/install.sh
@@ -5,19 +5,14 @@ curl -s -L https://iterm2.com/shell_integration/bash -o shell/iterm2_shell_integ
 curl -s -L https://iterm2.com/shell_integration/zsh -o shell/iterm2_shell_integration.zsh
 
 if [ -n "$LINUX" ]; then
-  # if type zsh >/dev/null; then
-  #   echo "zsh already installed, skipping"
-  # else
-  #   sudo apt-get install -y zsh
-  # fi
+  if ! test -f ~/.dotfiles/antigen.zsh; then
+    curl -L git.io/antigen > ~/.dotfiles/antigen.zsh
+  fi
 
-  # type zsh >/dev/null && sudo chsh --shell /bin/zsh `whoami`
-
-  if type bat >/dev/null; then
-    echo "bat already installed, skipping"
-  else
-    curl -s -L https://github.com/sharkdp/bat/releases/download/v0.15.4/bat_0.15.4_amd64.deb -o /tmp/bat.deb && \
+  if ! type bat >/dev/null; then
+    curl -s -L https://github.com/sharkdp/bat/releases/download/v0.18.1/bat_0.18.1.amd64.deb -o /tmp/bat.deb && \
       sudo dpkg -i /tmp/bat.deb
   fi
 
+  apt-get install -y fswatch zsh
 fi
