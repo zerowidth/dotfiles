576e33d8ea13a7f48b85b662b4ec0a1f6814d6a0
Nathan Witmer <nathan@zerowidth.com>
2021-05-21 09:22:04 -0600

    install fzf to permanent location

--- fzf/install.sh
+++ fzf/install.sh
@@ -1,4 +1,4 @@
 [ -z "$LINUX" ] && exit 0
 
-[ -d /tmp/fzf ] || git clone --depth 1 https://github.com/junegunn/fzf.git /tmp/fzf
-/tmp/fzf/install --all --no-update-rc
+[ -d ~/.fzf ] || git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all --no-update-rc
