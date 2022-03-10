6e7e5877466fc98556627e7be39865b42e4faa22
Nathan Witmer <nathan@zerowidth.com>
2022-03-09 20:13:53 -0700

    don't exit when using zsh

--- dot_secrets.sh
+++ dot_secrets.sh
@@ -1,6 +1,7 @@
 #!/bin/env bash
 
-[[ -d ${HOME}/Dropbox/sync/private ]] || exit 0
-for script in $(find ${HOME}/Dropbox/sync/private -name "*.sh"); do
-  source "$script"
-done
+if [[ -d ${HOME}/Dropbox/sync/private ]]; then
+  for script in $(find ${HOME}/Dropbox/sync/private -name "*.sh"); do
+    source "$script"
+  done
+fi
