4292826a1b04632166e94acc88115c421b86abd3
Nathan Witmer <nathan@zerowidth.com>
2023-06-21 18:40:40 -0600

    using icloud, not dropbox now

--- dot_secrets.sh
+++ dot_secrets.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if [[ -d ${HOME}/Dropbox/sync/private ]]; then
-  for script in $(find ${HOME}/Dropbox/sync/private -name "*.sh"); do
+if [[ -d ${HOME}/Documents/sync/private ]]; then
+  for script in $(find ${HOME}/Documents/sync/private -name "*.sh"); do
     source "$script"
   done
 fi
